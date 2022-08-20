2e199e10f36d58cd5f5ceccb0c529cfc8d82b3f3
Silvia Colmenares <Silvianohemy38@gmail.com>
2022-08-20 14:47:45 -0300

    Arreglando fallo

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT__APP_apiKey,
-  authDomain: process.env.REACT__APP_authDomain,
-  projectId: process.env.REACT__APP_projectId,
-  storageBucket: process.env.REACT__APP_storageBucket,
-  messagingSenderId: process.env.REACT__APP_messagingSenderId,
-  appId: process.env.REACT__APP_appId
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
